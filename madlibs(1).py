7787b738bf91453552784960b8dda3b987884dc5
Noé G <noogux@gmail.com>
2022-10-06 17:01:37 -0400

    Add files via upload

---
+++ madlibs(1).py
@@ -0,0 +1,16 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+#formatos
+# print ("aprender a programar con " + (var)) --string  luego concat seguido de la variable
+# print ("aprender a programar con {}".format((var)) -- methodo format
+# print (f"aprender a programar con {var}) -- metodo f-string
+
+#MadLibs
+palabra1 = input("ingrese 1er palabra: ")
+palabra2 = input("ingrese 2da palabra: ")
+palabra3 = input("ingrese 3era palabra: ")
+palabra4 = input("ingrese 4ta palabra: ")
+
+madlibs = f"Programar es tan {palabra1}. Me hace sentir {palabra2}, pero a veces {palabra3}. De igual forma yo siempre seguire {palabra4}!."
+print(madlibs)
